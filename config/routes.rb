d7373c89fc569d991b9dbeed41ab6f699c90d233
Elias Kirui <eliaskips1@gmail.com>
2021-11-19 19:26:47 +0300

    removed comments on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  root 'articles#index'
 
   resources :articles do
-  resources :comments
+    resources :comments
   end
-  #we've replaced the next two lines with resources
-  # get "/articles", to: "articles#index"
-  # get "articles/:id", to: "articles#show"
-
 end
