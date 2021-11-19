5bd2eb90bfc87aa87719fb8db963cd9979d6bb6f
Elias Kirui <eliaskips1@gmail.com>
2021-11-19 18:59:35 +0300

    added quite a number of features to the blog app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "articles/:id", to: "articles#show"
 
-  resources :articles
+  resources :articles do
+  resources :comments
+  end
+  #we've replaced the next two lines with resources
+  # get "/articles", to: "articles#index"
+  # get "articles/:id", to: "articles#show"
+
 end
